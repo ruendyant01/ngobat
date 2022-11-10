a66c315116b1d5273064dcb97d66bd3bcd3d8ee9
ruendyant01 <ruendyant.mayraclle@gmail.com>
2022-11-10 21:13:02 +0700

    5

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ function useEdit() {
     let getNama = document.querySelector("input#exampleInputEmail"); //input nama
     let getStock = document.querySelector("input#exampleInputPassword"); // input stock
     let getHarga = document.querySelector("input#exampleInputPassword2"); // input harga
+    if(!(getImage.value || getNama.value || getStock.value || getHarga.value)) {
+        alert("data tidak boleh ada yang kosong.Coba lagi");
+        return 0;
+    }
+
+    image.src = getImage.value;
+    nama.innerHTML = getNama.value;
+    stock.innerHTML = "Stock : "+getStock.value;
+    getHarga.innerHTML = "Harga : "+getHarga.value;
+
+    getImage.value = "";
+    getNama.value = "";
+    getStock.value = "";
+    getHarga.value = "";
 }
 
 function getId(id) {
@@ -19,13 +33,41 @@ function getId(id) {
 
 //fungsi delete
 function useDelete(id) {
-
+    let data= document.getElementById(id);
+    data.remove();
 }
 
 //fungsi create
 function useCreate() {
-    let setImage = document.querySelector("input#setImageD"); //input image
-    let setNama = document.querySelector("input#setNamaD"); // input nama
-    let setHarga = document.querySelector("input#setHargaD"); //input harga
-    let setStock = document.querySelector("input#setStockD"); // input stock
+    let setImage = document.querySelector("input#setImageD").value; //input image
+    let setNama = document.querySelector("input#setNamaD").value; // input nama
+    let setHarga = document.querySelector("input#setHargaD").value; //input harga
+    let setStock = document.querySelector("input#setStockD").value; // input stock
+    document.querySelector("input#setStockD").value = "";
+    document.querySelector("input#setHargaD").value = "";
+    document.querySelector("input#setNamaD").value = "";
+    document.querySelector("input#setImageD").value = "";
+
+    if(!setImage || !setNama || !setHarga || !setStock) {
+        alert("data tidak boleh ada yang kosong.Coba lagi");
+        return 0;
+    }
+
+    let cards = document.querySelector("div.cards");
+    let temp = `
+    <div class="card" style="width: 18rem;" id="${counter}">
+        <img src="${setImage}" class="card-img-top-${counter}" alt="${setNama}">
+        <div class="card-body">
+          <h5 class="card-title-${counter}">${setNama}</h5>
+          <p class="card-text-${counter}">Stock : ${setStock}</p>
+          <p class="card-text harga-${counter}">Harga : ${setHarga}</p>
+          <button id="${counter}" onclick="getId(this.id)" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+            Edit
+          </button>
+          <button type="button" class="btn delete btn-danger" id="${counter}" onclick="useDelete(this.id)">Delete</button>
+        </div>
+      </div>
+    `;
+    cards.innerHTML += temp;
+    counter++;
 }
